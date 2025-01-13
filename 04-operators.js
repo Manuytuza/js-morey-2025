7516fee336337886182b5eabcd21199ecca80169
Batman <alejo.ytuza@gmail.com>
2025-01-12 20:41:19 -0500

    fin 12primer dia

---
+++ 04-operators.js
@@ -0,0 +1,30 @@
+// operator aritmetico
+let a = 5
+let b = 10
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b) //modulo resto de la division
+console.log(a**b) // exponencial
+a++ //incremento
+console.log(a)
+b-- //decremento
+console.log(b)
+
+// operator de asignacion
+
+let myVariable = 2
+console.log(myVariable)
+myVariable += 2 // Suma con asignación
+console.log(myVariable)
+
+myVariable -= 2 // Resta con asignación
+myVariable *= 2 // Multiplicación con asignación
+myVariable /= 2 // División con asignación
+myVariable %= 2 // Módulo con asignación
+myVariable **= 2 // Exponente con asignación
+
+//operadores de comparacion
+// == compara valores
+// === compara valor y tipo
